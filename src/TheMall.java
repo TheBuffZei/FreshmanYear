80b580356f1d22cd5da83068314f44e5a922a68a
TheBuffZei <1aarushmyadam@gmail.com>
2022-09-08 22:19:38 +0530

    made them to strings, big mistake for billing.

--- src/TheMall.java
+++ src/TheMall.java
@@ -25,46 +25,51 @@ public class TheMall {
 
         //display floor
         if(f == 1) {
-            System.out.println("You are now in the Kids Wear Section.");
+            System.out.println("You are now in the Kids Wear Section");
         }
         else if(f == 2) {
-            System.out.println("You are now in the Ladies Wear Section.");
+            System.out.println("You are now in the Ladies Wear Section");
         }
         else if(f == 3) {
-            System.out.println("You are now in the Designer Sarees Section.");
+            System.out.println("You are now in the Designer Sarees Section");
         }
         else if(f == 4) {
-            System.out.println("You are now in the Men's Wear Section.");
+            System.out.println("You are now in the Men's Wear Section");
         }
         else{
-            System.out.println("We currently do not have that amount of floors, sorry.");
+            System.out.println("We currently do not have that amount of floors, sorry");
         }
-/*list of items available
-        int k_hoodies = 1000;
-        int k_pajamas = 880;
-        int k_shirts = 700;
-        int k_trousers = 700;
-        int k_denim_outfit = 1200;
-        int k_skirts = 650;
-        int k_footwear = 600;
 
-        int l_skirts = 800;
-        int l_kurta = 500;
-        int l_pyjama = 400;
-        int l_saree = 300;
+        //list of items available
+        //kids
+        String k_aisle1 = "Hoodies for 1000 Credits";
+        String k_aisle2 = "Pajamas for 800 Credits";
+        String k_aisle3 = "Trousers for 700 Credits";
+        String k_aisle4 = "Denim Wear for 1200 Credits";
+        String k_aisle5 = "Shirts for 650 Credits";
+        String k_aisle6 = "Shirts for 700 Credits";
+        String k_aisle7 = "Footwear for 600 Credits";
+        //ladies
+        String l_aisle1 = "Skirts for 800 Credits";
+        String l_aisle2 = "Kurtas for 500 Credits";
+        String l_aisle3 = "Pajamas for 900 Credits";
+        String l_aisle4 = "Sarees for 300 Credits";
+        //designer
+        String d_aisle1 = "Half Sarees for 3000 Credits";
+        String d_aisle2= "Lehangas for 5500 Credits";
+        String d_aisle3 = "Pattu Sarees for 30000 Credits";
+        //mens
+        String m_aisle1 = "Jeans for 1500 Credits";
+        String m_aisle2 = "Shirts for 1000 Credits";
+        String m_aisle3 = "Kurtas for 1000 Credits";
+        String m_aisle4 = "Coats for 3500 Credits";
+        String m_aisle5= "Ties for 500 Credits";
+        String m_aisle6 = "Footwear for 1000 Credits";
 
-        int d_halfSaree = 3000;
-        int d_lehanga = 5500;
-        int d_pattuSaree =30000;
-
-        int m_jeans = 0;
-        int m_shirts = 0;
-        int m_kurtas = 0;
-        int m_coats = 0;
-        int m_ties = 0;
-        int m_footwear = 0;
-*/
         //output the items available
-
+        if(f == 1) {
+            System.out.println("In the Kids Section, we have:");
+            System.out.println("Aisle 1: "+k_aisle1+"\nAisle 2: "+k_aisle2+"\nAisle 3: "+k_aisle3+"\nAisle 4: "+k_aisle4);
+        }
     }
 }
